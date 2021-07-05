507d9f0f00f0452b139f6839338dee8d90bebc99
Lari Oncescu <larioncescu@yahoo.com>
2021-07-05 20:09:55 +0300

    Add formik component to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import * as S from './styles'
+import { Formik } from 'formik'
+import * as yup from 'yup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.StyledApp>
+      <Formik
+      initialValues = {{
+        catYears: 0,
+        dogYears: 0
+      }}
+      validationSchema={yup.object().shape({
+        catYears: yup.number().min(1).max(25),
+        dogYears: yup.number().min(1).max(30)
+      })}
+      onSubmit={(values) => console.log(values)}
+      >
+
+      </Formik>
+    </S.StyledApp>
+
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledApp = styled.div`
+  background-color: blueviolet;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  color: white;
+`
